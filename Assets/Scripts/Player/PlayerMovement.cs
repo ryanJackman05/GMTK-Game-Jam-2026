e05af6231030465fd3d1a29344848ba99d105508
ryanjackman05 <20108256@mail.wit.ie>
2026-07-23 22:06:51 +0100

    basic movement and collision added. player prefab created

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] Vector2 moveVector;
+    
+    [SerializeField] Animator animator;
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 move = value.Get<Vector2>();
         
+        moveVector = move;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.velocity = (moveVector * speed);
+        if (moveVector.magnitude > 0){
+            // TODO animator.SetBool("walking", true);
+        }
         
     }
 }
