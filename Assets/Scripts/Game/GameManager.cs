ec80d7c976d94d0709c43979b88d43d762283445
ryanjackman05 <20108256@mail.wit.ie>
2026-07-25 14:04:02 +0100

    Added TMP package. SHit ton of assets imported, sorry

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject gameOverMenu;
+    [SerializeField] GameObject inventoryScreen;
     
-    public static PlayerController player;
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] TextMeshProUGUI dialogueText;
+    //[SerializeField] TextMeshProUGUI characterName;
+    
+    public static PlayerController player; // set by Player on spawn
     // Start is called before the first frame update
     void Start()
     {
@@ -19,4 +25,18 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
 }
